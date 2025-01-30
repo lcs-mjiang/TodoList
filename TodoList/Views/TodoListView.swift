455accab7f992d326f1306c36fc00da2ad8f500c
Michael Jiang <mjiang@lcs.on.ca>
2025-01-30 15:22:40 -0500

    Organize project structure.

---
+++ TodoList/Views/TodoListView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Michael on 2025/1/30.
+//
+
+import SwiftUI
+
+struct TodoListVIew: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TodoListVIew()
+}
---
+++ TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Michael on 2025/1/30.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
