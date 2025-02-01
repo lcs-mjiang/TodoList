0290d38a1aa0707d0821a56d5a9aa8779a7837ff
Michael Jiang <mjiang@lcs.on.ca>
2025-02-01 15:47:31 -0500

    Adjusted the view to work with the model context instead of an in-memory array.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -5,6 +5,7 @@
 //  Created by Michael on 2025/1/30.
 //
 import SwiftData
+
 import SwiftUI
 
 struct LandingView: View {
@@ -21,31 +22,23 @@ struct LandingView: View {
     @Environment(\.modelContext) var modelContext
     
     // The list of to do item
-    @State var todos: [TodoItem] = exampleItems
+    @Query var todos: [TodoItem]
+    
     // MARK: Computed properties
     var body: some View {
         NavigationView {
             
             VStack {
                 
-                List($todos) { $todo in
-                    
-                    ItemView(currentItem: $todo)
-                    //ItemView(currentItem: firstItem)
-                    //ItemView(currentItem: secondItem)
-                    //ItemView(currentItem: thirdItem)
-                    // Delete a to-do item
-                        .swipeActions {
-                            Button(
-                                "Delete",
-                                role: .destructive,
-                                action: {
-                                    delete(todo)
-                                }
-                            )
-                        }
-                    
+                List {
+                    ForEach(todos) { todo in
+                        
+                        ItemView(currentItem: todo)
+                 
+                    }
+                    .onDelete(perform: removeRows)
                 }
+                .searchable(text: $searchText)
                 .searchable(text: $searchText)
                 
                 HStack {
@@ -73,14 +66,18 @@ struct LandingView: View {
             done: false
         )
 
-        // Append to the array
-        todos.append(todo)
+        // Use the model context to insert the new to-do
+        modelContext.insert(todo)
     }
-    func delete(_ todo: TodoItem) {
+    func removeRows(at offsets: IndexSet) {
         
-        // Remove the provided to-do item from the array
-        todos.removeAll { currentItem in
-            currentItem.id == todo.id
+        // Accept the offset within the list
+        // (the position of the item being deleted)
+        //
+        // Then ask the model context to delete this
+        // for us, from the 'todos' array
+        for offset in offsets {
+            modelContext.delete(todos[offset])
         }
     }
 }
