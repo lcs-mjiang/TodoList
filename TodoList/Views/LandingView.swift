c68ab6b806f02951c5fd49c40ef7e841722ff2ef
Michael Jiang <mjiang@lcs.on.ca>
2025-02-01 15:41:02 -0500

    Added the model context to the landing view so we are ready to perform creations, deletions, modifications, etc.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Michael on 2025/1/30.
 //
-
+import SwiftData
 import SwiftUI
 
 struct LandingView: View {
@@ -16,6 +16,9 @@ struct LandingView: View {
     
     // The search text
     @State var searchText = ""
+    
+    // Access the model context (required to do additions, deletions, updates, et cetera)
+    @Environment(\.modelContext) var modelContext
     
     // The list of to do item
     @State var todos: [TodoItem] = exampleItems
